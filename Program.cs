2eeceefff9769ea4cccdd67991010a32c5e1fbfb
Veronika <vvnika@mail.ru>
2022-07-07 02:29:51 +1100

    add "Домашняя работа _Задача 23_Семинар_3"

--- Program.cs
+++ Program.cs
@@ -23,6 +23,7 @@ CheckingNumber(number);
 }
 else Console.WriteLine($"Введи правильное число");
 */
+/*
 // Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
@@ -52,3 +53,38 @@ double Decision(double x1, double x2,
 double segmentLength = Math.Round(Decision(x1, x2, y1, y2, z1, z2), 2);
 
 Console.WriteLine($"Длина отрезка  {segmentLength}");
+*/
+//Задача 23: Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("\n Задача 23");
+Console.Write("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube)
+{
+    int counter = 0;
+    int length = cube.Length;
+    while (counter < length)
+    {
+        cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+        counter++;
+    }
+}
+
+void PrintArry(int[] coll)
+{
+    int count = coll.Length;
+    int index = 0;
+    while (index < count)
+    {
+        Console.Write(coll[index] + " ");
+        index++;
+    }
+}
+
+int[] arry = new int[cube + 1];
+Cube(arry);
+PrintArry(arry);
